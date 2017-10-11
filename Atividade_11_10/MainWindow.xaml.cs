14f367e227946ff88fb294408e2df47e1cef9113
lab4_6 <lab4_6@DESKTOP-QL6G9F2>
2017-10-11 18:37:17 -0300

    Add project files.

---
+++ Atividade_11_10/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Atividade_11_10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region contrutor
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+#endregion
+
+
+
+        private void Testar(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Voce escolheu " + cboxTeste.Text, "Aviso",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
