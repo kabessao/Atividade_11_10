65c8795f3ba5baadbe9c0668731cf0b341c5e57f
Henrique Soares da Silva <henrique.silva533@etec.sp.gov.br>
2017-10-11 21:35:25 -0300

    Feita a funcionalidade de cada botão

--- calculadora/MainWindow.xaml.cs
+++ calculadora/MainWindow.xaml.cs
@@ -24,5 +24,69 @@ namespace calculadora
         {
             InitializeComponent();
         }
+
+        private void Operacao(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtValor1.Text))
+            {
+                txtValor1.Text = txtValor2.Text + " " + (sender as Button).Content.ToString();
+                txtValor2.Text = "0";
+            }
+            else if (txtValor2.Text != "0")
+            {
+                txtValor1.Text = VerOpção(txtValor1.Text) + " " + (sender as Button).Content.ToString();
+                txtValor2.Text = "0";
+            }
+
+            if (!string.IsNullOrWhiteSpace(txtValor2.Text))
+            {
+                txtValor1.Text = txtValor1.Text.Remove(txtValor1.Text.Length - 1) + (sender as Button).Content.ToString();
+            }
+
+        }
+
+        private void Igual(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtValor1.Text))
+            {
+                txtValor2.Text = VerOpção(txtValor1.Text);
+                txtValor1.Text = "";
+            }
+        }
+
+        private void Numero(object sender, RoutedEventArgs e)
+        {
+            txtValor2.Text += (sender as Button).Content.ToString();
+        }
+
+        private string VerOpção(string op)
+        {
+
+            string opcao = op.Substring(op.Length - 1);
+            double valor1 = double.Parse(txtValor1.Text.ToString().Substring(0, txtValor1.Text.Length - 1)),
+                valor2 = double.Parse(txtValor2.Text);
+            if (op.IndexOf('+') > 0)
+            {
+                return $"{valor1 + valor2}";
+            }
+            else if (op.IndexOf('-') > 0)
+            {
+                return $"{valor1 - valor2}";
+            }
+            else if (op.IndexOf('x') > 0)
+            {
+                return $"{valor1 * valor2}";
+            }
+            else if (op.IndexOf('/') > 0)
+            {
+                if (valor1 != 0 && valor2 != 0)
+                {
+                    return $"{valor1 / valor2}";
+                }
+            }
+
+            return "0";
+
+        }
     }
 }
